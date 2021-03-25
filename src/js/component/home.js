30a39795ca9dfccc3bb6ba582b2ea6107a1ebf25
Mauriel <mauriel.azofeifa@uned.cr>
2021-03-25 22:11:17 +0000

    music-player-ready

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,118 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.audio = null;
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+		this.state = {
+			currentIndex: 0,
+			songs: [
+				{
+					title: "South Park",
+					id: "south-park",
+					author: "Kyle",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/south-park.mp3"
+				},
+				{
+					title: "Thunder Cats",
+					id: "thundercats",
+					author: "Moonra",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/thundercats.mp3"
+				},
+				{
+					title: "X-Men",
+					id: "x-men",
+					author: "Profesor",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/x-men.mp3"
+				}
+			]
+		};
+	}
+
+	componentDidMount() {
+		this.pauseButton.style.display = "none";
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+			.then(response => response.json())
+			.then(songs => this.setState({ songs }));
+	}
+
+	changeTrack(i) {
+		this.setState({ currentIndex: i });
+		this.audio.current.pause();
+		this.audio.current.load();
+		this.audio.current.play();
+	}
+
+	play = i => {
+		var url = this.state.songs[i].url;
+		const songURL = "https://assets.breatheco.de/apis/sound/" + url;
+		this.audio.src = songURL;
+		this.audio.play();
+		this.playButton.style.display = "none";
+		this.pauseButton.style.display = "inline-block";
+		this.setState({ currentIndex: i });
+	};
+
+	pause = () => {
+		this.audio.pause();
+		this.playButton.style.display = "inline-block";
+		this.pauseButton.style.display = "none";
+	};
+
+	render() {
+		const liList = this.state.songs.map((song, index) => {
+			return (
+				<li
+					className={
+						this.state.currentIndex == index ? "active" : "body"
+					}
+					key={index}
+					onClick={() => this.play(index)}>
+					<div className="row">
+						<span className="number">{index + 1}</span>
+						<span className="title">{song.name}</span>
+					</div>
+				</li>
+			);
+		});
+
+		const audioPlayer = (
+			<>
+				<div className="buttons">
+					<button
+						onClick={() => this.play(this.state.currentIndex - 1)}>
+						<i className="fa fa-caret-left" aria-hidden="true" />
+					</button>
+					<button
+						ref={element => (this.playButton = element)}
+						onClick={() => this.play(this.state.currentIndex)}>
+						<i className="fa fa-play" aria-hidden="true" />
+					</button>
+					<button
+						ref={element => (this.pauseButton = element)}
+						onClick={() => this.pause(this.state.currentIndex)}>
+						<i className="fa fa-pause" aria-hidden="true" />
+					</button>
+					<button
+						onClick={() => this.play(this.state.currentIndex + 1)}>
+						<i className="fa fa-caret-right" aria-hidden="true" />
+					</button>
+				</div>
+				<div className="tracker">
+					<audio controls ref={element => (this.audio = element)} />
+				</div>
+			</>
+		);
+
+		return (
+			<>
+				{audioPlayer}
+				<ul>{liList}</ul>
+			</>
+		);
+	}
 }
